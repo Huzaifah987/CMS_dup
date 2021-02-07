75bbff374c2bf8e432a4be4ecf5e8d209e633512
Huzaifah987 <amirhuzaifah987@gmail.com>
2021-02-07 17:40:45 +0800

    Init Initialize

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -14,35 +14,56 @@
       <th>Delete</th>
     </tr>
   </thead>
-  <tbody>
-    <?php
-      $query = "SELECT * FROM posts";
-      $query_select_all_post = mysqli_query($link, $query);
+  <tbody >
+      <?php
+        $query = "SELECT * FROM posts";
+        $select_posts = mysqli_query($link, $query);
 
-      while ($row = mysqli_fetch_assoc($query_select_all_post)) {
-        $post_id = $row['post_id'];
-        $post_title = $row['post_title'];
-        $post_author = $row['post_author'];
-        $post_image = $row['post_Image'];
-        $post_comment = $row['post_content'];
-        $post_status = $row['post_status'];
-        $post_tags = $row['post_tags'];
-        $post_date = $row['post_date'];
+        while ($row = mysqli_fetch_assoc($select_posts)) {
+          $post_id = $row['post_id'];
+          $post_title = $row['post_title'];
+          $post_author = $row['post_author'];
+          $post_date = $row['post_date'];
+          $post_image = $row['post_Image'];
+          $post_category = $row['post_category_id'];
+          $post_tags = $row['post_tags'];
+          $post_comment= $row['post_content'];
+          $post_status = $row['post_status'];
 
-        echo "<tr>";
-        echo "<td>{$post_id}</td>";
-        echo "<td>{$post_title}</td>";
-        echo "<td>{$post_author}</td>";
-        echo "<td><img width='100'  src='../images/{$post_image}'</td>";
-        echo "<td> </td>";
-        echo "<td>{$post_tags}</td>";
-        echo "<td>{$post_comment}</td>";
-        echo "<td>{$post_status}</td>";
-        echo "<td>{$post_date}</td>";
-        echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-        echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
-        echo "</tr>";
-      }
-     ?>
+          echo "<tr>";
+          echo "<td>{$post_id}</td>";
+          echo "<td>{$post_title}</td>";
+          echo "<td>{$post_author}</td>";
+          echo "<td><img width='100'  src='../images/{$post_image}'</td>";
+
+          $query_category ="SELECT * FROM categories WHERE cat_id = {$post_category}";
+          $query_select_id = mysqli_query($link, $query_category);
+
+          while ($row = mysqli_fetch_assoc($query_select_id)) {
+             $cat_id = $row['cat_id'];
+             $cat_title = $row['cat_title'];
+          }
+
+          echo "<td>{$cat_title}</td>";
+          echo "<td>{$post_tags}</td>";
+          echo "<td>{$post_comment}</td>";
+          echo "<td>{$post_status}</td>";
+          echo "<td>{$post_date}</td>";
+          echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+          echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+          echo "</tr>";
+        }
+       ?>
   </tbody>
 </table>
+
+
+<?php
+  if (isset($_GET['delete'])) {
+    $post_id = $_GET['delete'];
+
+    $query = "DELETE FROM posts WHERE post_id = $post_id";
+    $delete_query = mysqli_query($link, $query);
+    header("Location: posts.php");
+  }
+ ?>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,36 +1,52 @@
-<div class="col-md-4">
 
+<div class="col-md-4">
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
+        <form class="" action="search.php" method="post">
         <div class="input-group">
-            <input type="text" class="form-control">
+            <input type="text" name='search' class="form-control">
             <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
+                <button name='submit' class="btn btn-default" type="submit">
                     <span class="glyphicon glyphicon-search"></span>
             </button>
             </span>
         </div>
+          </form>
+          <!-- search form -->
+
         <!-- /.input-group -->
     </div>
 
+
+
+
     <!-- Blog Categories Well -->
     <div class="well">
+      <?php
+        $query = "SELECT * FROM categories";
+        $select_categories_sidebar = mysqli_query($link, $query);
+       ?>
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
-              <?php
-                $query = "SELECT * FROM categories";
-                $cat_query = mysqli_query($link, $query);
-
-                while ($row = mysqli_fetch_assoc($cat_query)) {
-                  $cat_title = $row['cat_title'];
-                  echo "<li><a href='#'>{$cat_title}</a></li>";
-                }
-               ?>
+                  <?php
+                    while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+                    echo "<li><a href='./category.php?category=$cat_id'>{$cat_title}</a></li>";
+                  }
+                   ?>
                 </ul>
             </div>
+
+            <!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+
+            </div>
+
+
             <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
@@ -38,13 +54,7 @@
 
     <!-- Side Widget Well -->
     <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+      <?php include 'widget.php' ?>
     </div>
 
 </div>
-
-</div>
-<!-- /.row -->
-
-<hr>
