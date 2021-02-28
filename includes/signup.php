787e927fba48fa325b1c234c3f2ed79a1689f1b0
Huzaifah987 <amirhuzaifah987@gmail.com>
2021-02-28 21:57:34 +0800

    Signup

--- includes/signup.php
+++ includes/signup.php
@@ -1,3 +1,32 @@
+<?php
+  include 'db.php';
+  if (isset($_POST['sign-up'])) {
+    $user_firstname        = $_POST['user_firstname'];
+    $user_lastname       = $_POST['user_lastname'];
+    $username       = $_POST['username'];
+
+    $user_email        = $_POST['user_email'];
+    $user_password  = $_POST['user_password'];
+
+    $user_firstname = $user_lastname = $username = $user_email = $user_password = "";
+
+    $hashFormat= "$2y$10$";
+    $salt = "iusesomecrazystings222";
+    $hashF_and_salt= $hashFormat . $salt;
+
+    $user_password = mysqli_real_escape_string($link, $user_password);
+    $user_email = mysqli_real_escape_string($link, $user_email);
+
+    $user_password= crypt($user_password,  $hashF_and_salt);
+
+
+    $query = "INSERT INTO users(user_firstname, user_lastname,username,user_email,user_password)";
+    $query .= "VALUES('$user_firstname','$user_lastname','$username','$user_email','$user_password')";
+
+    $create_user_query = mysqli_query($link, $query);
+  }
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,23 +43,29 @@
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
         <form action="" method="post">
+          <div class="mb-3">
+            <label  class="form-check-label" for="Title">First Name</label>
+            <input class="form-control" type="text" name="user_firstname" value="">
+          </div>
+          <div class="mb-3">
+            <label  class="form-check-label" for="Title">Last Name</label>
+            <input class="form-control" type="text" name="user_lastname" value="">
+          </div>
+          <div class="mb-3mb-3">
+            <label  class="form-check-label" for="Title">Username</label>
+            <input  class="form-control" type="text" name="username" value="">
+          </div>
+          <div class="mb-3">
+            <label  class="form-check-label" for="Title">E-mail</label>
+            <input class="form-control" type="text" name="user_email" value="">
+          </div>
+          <div class="mb-3">
+            <label  class="form-check-label" for="Title">Password</label>
+            <input class="form-control" type="password" name="user_password" value="">
+          </div>
+          <br>
             <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="">
-                <span class="help-block"></span>
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" value="">
-                <span class="help-block"></span>
-            </div>
-            <div class="form-group ">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" value="">
-                <span class="help-block"></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="submit" class="btn btn-primary" name="sign-up" value="Submit">
                 <input type="reset" class="btn btn-default" value="Reset">
             </div>
             <p>Already have an account? <a href="../index.php">Login here</a>.</p>
